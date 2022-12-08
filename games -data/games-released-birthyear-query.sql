19e46bd16850147bd44a72e2ecd03e9395d6d058
[sean] <[seankody3204@gmail.com]>
2022-12-08 00:18:32 -0600

    add games data project

---
+++ games -data/games-released-birthyear-query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM games WHERE first_release_date BETWEEN '2004-03-02 00:00:00' AND '2004-12-31 23:59:59' ORDER BY name;
---
+++ games -data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (70491089108, 1, 'Hello absent students !')
+
+--UPDATE sandbox SET double_value = 2.1543054 WHERE id = 125;
+
+--DELETE FROM sandbox WHERE id = 100;
+
+SELECT * FROM sandbox;
